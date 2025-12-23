48f0a0fac1252ad8cff24d14ca91d546628333b3
ZAIDALIKHAN <39004623+ZAIDALIKHAN@users.noreply.github.com>
2025-12-23 12:47:31 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "zaidu1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "zaidu1"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "zaidu2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "zaidu2"
     Environment = "dev"
   }
 }
